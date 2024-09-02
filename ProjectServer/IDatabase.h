814f012d62ca13730a89715e6c1da576b8e80527
chanoch <c0548519466@gmai.com>
2024-09-02 15:06:16 +0300

    marge

--- ProjectServer/IDatabase.h
+++ ProjectServer/IDatabase.h
@@ -3,7 +3,7 @@
 class IDatabase
 {
 public:
-	bool open();
+	virtual bool open()=0;
 	//bool close();
 	virtual int doesUserExist(std::string) = 0;
 	virtual int doesPasswordMatch(std::string, std::string)=0;
